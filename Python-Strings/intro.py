4bc9e69ba867c1252d292fad9c957752fe5f3f73
jethrocruz <jethro.cruz@gmail.com>
2024-03-03 14:57:09 -0500

    3-3-1

--- Python-Strings/intro.py
+++ Python-Strings/intro.py
@@ -4,8 +4,9 @@
 
 # print(help(str.lower))
 
+import re
 
-message = 'Hello World'
+message = 'Hello World i am pleased to see you'
 
 print(message.lower())
 
@@ -16,4 +17,21 @@ print(len(message))
 print(message.count('l'))
 
 
-print(messa)
+print(message.find('to'))
+
+print(message.find('World'))
+
+message = message.replace('World', '      Universe')
+
+print(message.replace('World', 'Universe')) # temp
+
+##IGNORE CASE using import re
+# import re
+
+# message = 'Hello World i am pleased to see you'
+# old_substring = 'world'
+# new_substring = 'universe'
+
+# # Use re.IGNORECASE flag for case-insensitive matching
+# result = re.sub(re.escape(old_substring), new_substring, message, flags=re.IGNORECASE)
+# print(result)
