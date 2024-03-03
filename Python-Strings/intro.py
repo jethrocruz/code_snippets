97670ba7b4cf829b68a6a7d05a79c5b02504b215
jethrocruz <jethro.cruz@gmail.com>
2024-03-03 15:18:48 -0500

    3-3-1

--- Python-Strings/intro.py
+++ Python-Strings/intro.py
@@ -1,29 +1,29 @@
 
-# greeting = 'Hello'
-# name = 'Michael'
+# # greeting = 'Hello'
+# # name = 'Michael'
 
-# print(help(str.lower))
+# # print(help(str.lower))
 
-import re
+# import re
 
-message = 'Hello World i am pleased to see you'
+# message = 'Hello World i am pleased to see you'
 
-print(message.lower())
+# print(message.lower())
 
-print(message.upper())
+# print(message.upper())
 
-print(len(message))
+# print(len(message))
 
-print(message.count('l'))
+# print(message.count('l'))
 
 
-print(message.find('to'))
+# print(message.find('to'))
 
-print(message.find('World'))
+# print(message.find('World'))
 
-message = message.replace('World', '      Universe')
+# message = message.replace('World', '      Universe')
 
-print(message.replace('World', 'Universe')) # temp
+# print(message.replace('World', 'Universe')) # temp
 
 ##IGNORE CASE using import re
 # import re
@@ -35,3 +35,26 @@ print(message.replace('World', 'Universe')) # temp
 # # Use re.IGNORECASE flag for case-insensitive matching
 # result = re.sub(re.escape(old_substring), new_substring, message, flags=re.IGNORECASE)
 # print(result)
+
+#USING f STRING PLACE HOLDER
+
+# greeting = 'Hello'
+# name = " mike"
+
+# message = '{}, {}. Welcome'.format(greeting, name)
+
+# print(message)
+
+
+name1 = input('Enter your name: ')
+greet1= input('language to welcome: ')
+
+# gmessage = '{} {}, My name is Jet'.format(greet1,name1)
+
+# print(gmessage)
+
+
+message2 = f'{greet1} {name1.upper()}!, My name is Pedro'
+
+print(message2)
+
